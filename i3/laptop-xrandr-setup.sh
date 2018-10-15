1ac2784c7d86d03dd8c78a9d33ac98f970a64a54
Jonas Cosandey <jonas.cosandey@adfinis-sygroup.ch>
2018-10-15 13:45:42 +0200

    i3 multimedia keys configured

--- i3/laptop-xrandr-setup.sh
+++ i3/laptop-xrandr-setup.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
-#xrandr --output DP-1-2 --off --output DP-1-3 --off --output eDP-1 --off
-xrandr --output DP-1-2 --off --output DP-1-3 --off --output eDP-1 --auto --primary --auto
+xrandr --output DP-1-2 --off --output DP-1-3 --off --output eDP-1 --off
+xrandr --output DP1-2 --off --output DP1-3 --off --output eDP1 --auto --primary --auto
