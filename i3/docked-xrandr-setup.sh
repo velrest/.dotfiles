7382e7a2cb612f2e8030a00f72b37b93ae3caecb
Jonas Cosandey <jonas.cosandey@adfinis-sygroup.ch>
2019-02-26 09:21:48 +0100

    fix(xrandr-docked): Fix doccked xrandr setup

--- i3/docked-xrandr-setup.sh
+++ i3/docked-xrandr-setup.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-xrandr --output eDP1 --mode 1920x1080 --pos 3000x0 --rotate normal --output DP1-2 --primary --mode 1920x1080 --pos 1920x0 --rotate right --output HDMI2 --off --output HDMI1 --off --output DP1 --off --output DP1-3 --mode 1920x1080 --pos 0x0 --rotate normal --output DP2 --off --output DP1-1 --off
+xrandr --output DP1-2 --off	--output HDMI2 --off --output HDMI1 --off
+xrandr --output DP1-2 --primary --mode 1920x1080 --pos 1920x0 --rotate right --output DP1-3 --mode 1920x1080 --pos 0x0 --rotate normal
+xrandr --output eDP1 --mode 1920x1080 --pos 3000x0 --rotate normal  
